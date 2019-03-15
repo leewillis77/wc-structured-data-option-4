d47b1e41a4284f4dc85038b641ad91f823717f46
Lee Willis <lee@ademti-software.co.uk>
2019-03-15 11:27:36 +0000

    Support for priceValidUntil as per WC core implementation

--- wc-structured-data-option-4.php
+++ wc-structured-data-option-4.php
@@ -21,11 +21,16 @@ add_filter( 'woocommerce_structured_data_product', function( $markup, $product )
 	$variation_id = $data_store->find_matching_product_variation( $product, wp_unslash( $_GET ) );
 	$variation    = $variation_id ? wc_get_product( $variation_id ) : false;
 	if ( ! empty( $variation ) ) {
+		$price_valid_until = date( 'Y-12-31', current_time( 'timestamp', true ) + YEAR_IN_SECONDS );
+		if ( $variation->is_on_sale() && $variation->get_date_on_sale_to() ) {
+			$price_valid_until = date( 'Y-m-d', $variation->get_date_on_sale_to()->getTimestamp() );
+		}
 		$markup_offer = $markup['offers'][0];
 		unset( $markup_offer['lowPrice'] );
 		unset( $markup_offer['highPrice'] );
 		$markup_offer['@type'] = 'Offer';
 		$markup_offer['price'] = wc_format_decimal( $variation->get_price(), wc_get_price_decimals() );
+		$markup_offer['priceValidUntil'] = $price_valid_until;
 		$markup_offer['url'] = $variation->get_permalink();
 		$markup['offers'][0] = apply_filters( 'woocommerce_structured_data_product_offer', $markup_offer, $product );
 	}
