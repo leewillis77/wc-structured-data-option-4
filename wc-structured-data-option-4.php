276d7ba01ee6ba485f32935ac0f9fd582a720916
Lee Willis <lee@ademti-software.co.uk>
2020-05-26 13:58:57 +0100

    Various changes

--- wc-structured-data-option-4.php
+++ wc-structured-data-option-4.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/woocommerce/woocommerce/issues/17471
  * Description: Implements alternative structured data for variations as per the discussions on https://github.com/woocommerce/woocommerce/issues/17471.
  * Author: Lee Willis
- * Version: 0.1
+ * Version: 0.2
  * WC requires at least: 3.2.0
  * WC tested up to: 3.2.0
  * Author URI: http://www.leewillis.co.uk/
@@ -21,11 +21,18 @@ add_filter( 'woocommerce_structured_data_product', function( $markup, $product )
 	$variation_id = $data_store->find_matching_product_variation( $product, wp_unslash( $_GET ) );
 	$variation    = $variation_id ? wc_get_product( $variation_id ) : false;
 	if ( ! empty( $variation ) ) {
+		// Move the SKU away from the main product, and into the offer.
+		$markup['offers'][0]['sku'] = $markup['sku'];
+		unset( $markup['sku'] );
+		// Copy the existing offer and use it as the basis for a new one.
+		$markup_offer = $markup['offers'][0];
+
+		// Calculate the priceValidUntil date for the variation.
 		$price_valid_until = date( 'Y-12-31', current_time( 'timestamp', true ) + YEAR_IN_SECONDS );
 		if ( $variation->is_on_sale() && $variation->get_date_on_sale_to() ) {
 			$price_valid_until = date( 'Y-m-d', $variation->get_date_on_sale_to()->getTimestamp() );
 		}
-		$markup_offer = $markup['offers'][0];
+
 		unset( $markup_offer['lowPrice'] );
 		unset( $markup_offer['highPrice'] );
 		unset( $markup_offer['offerCount'] );
@@ -33,7 +40,8 @@ add_filter( 'woocommerce_structured_data_product', function( $markup, $product )
 		$markup_offer['price'] = wc_format_decimal( $variation->get_price(), wc_get_price_decimals() );
 		$markup_offer['priceValidUntil'] = $price_valid_until;
 		$markup_offer['url'] = $variation->get_permalink();
-		$markup['offers'][0] = apply_filters( 'woocommerce_structured_data_product_offer', $markup_offer, $product );
+		$markup_offer['sku'] = $variation->get_sku();
+		$markup['offers'][0] = apply_filters( 'woocommerce_structured_data_product_offer', $markup_offer, $variation );
 	}
 	return $markup;
 }, 9, 2);
